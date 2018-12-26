5f5291fd19f6f1ae59fac6a008b6bb22040d4683
MichaelTd <tsouchlarakis@gmail.com>
2018-12-26 20:10:49 +0200

    bash

--- cgi-bin/cgi_lib.sh
+++ cgi-bin/cgi_lib.sh
@@ -5,26 +5,26 @@ function print_headers {
 }
 
 function print_head {
-  printf "<head><title>${1}</title>"
-  printf "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>"
-  printf "<link rel='shortcut icon' href='../assets/favicon.ico'>"
-  printf "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>"
-  printf "<link href='../css/css.css' rel='stylesheet' type='text/css'>"
-  printf "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>"
-  printf "<script src='../js/js.js'></script>"
-  printf "<meta name='viewport' content='width=device-width, initial-scale=1.0'></head>"
+  printf "<head><title>${1}</title> 
+          <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+          <link rel='shortcut icon' href='../assets/favicon.ico'>
+          <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+          <link href='../css/css.css' rel='stylesheet' type='text/css'>
+          <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
+          <script src='../js/js.js'></script>
+          <meta name='viewport' content='width=device-width, initial-scale=1.0'></head>"
 }
 
 function print_footer {
-  printf "<footer>"
-  printf "<a href='https://plus.google.com/u/0/109449761845014157264'><i class='fa fa-google-plus fa-fw fa-1x'></i></a>"
-  printf "<a href='https://www.linkedin.com/in/michael-tsouchlarakis/'><i class='fa fa-linkedin fa-fw fa-1x'></i></a>"
-  printf "<a href='https://twitter.com/tsouchlarakismd'><i class='fa fa-twitter fa-fw fa-1x'></i></a>"
-  printf "<a href='https://github.com/michaeltd'><i class='fa fa-github fa-fw fa-1x'></i></a>"
-  printf "<a href='http://codepen.io/tdmichael/'><i class='fa fa-codepen fa-1x'></i></a>"
-  printf "<a href='https://www.freecodecamp.com/michaeltd/'><i class='fa fa-free-code-camp fa-fw fa-1x'></i></a>"
-  printf "<script type='text/javascript'>var year = new Date();document.write(year.getFullYear());</script>"
-  printf "<a href='https://dev.to/michaeltd'><img src='https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg' alt=\"Michael DT's DEV Profile\" height='30' width='30' /></a>"
-  printf "<script type='text/javascript'>var lm = new Date(document.lastModified);document.write('(Last Update: ' + lm.getFullYear() + '/' + (lm.getMonth()+1) + '/' + lm.getDate()+ ')');</script>"
-  printf "</footer>"
+  printf "<footer>
+          <a href='https://plus.google.com/u/0/109449761845014157264'><i class='fa fa-google-plus fa-fw fa-1x'></i></a>
+          <a href='https://www.linkedin.com/in/michael-tsouchlarakis/'><i class='fa fa-linkedin fa-fw fa-1x'></i></a>
+          <a href='https://twitter.com/tsouchlarakismd'><i class='fa fa-twitter fa-fw fa-1x'></i></a>
+          <a href='https://github.com/michaeltd'><i class='fa fa-github fa-fw fa-1x'></i></a>
+          <a href='http://codepen.io/tdmichael/'><i class='fa fa-codepen fa-1x'></i></a>
+          <a href='https://www.freecodecamp.com/michaeltd/'><i class='fa fa-free-code-camp fa-fw fa-1x'></i></a>
+          <script type='text/javascript'>var year = new Date();document.write(year.getFullYear());</script>
+          <a href='https://dev.to/michaeltd'><img src='https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg' alt=\"Michael DT's DEV Profile\" height='30' width='30' /></a>
+          <script type='text/javascript'>var lm = new Date(document.lastModified);document.write('(Last Update: ' + lm.getFullYear() + '/' + (lm.getMonth()+1) + '/' + lm.getDate()+ ')');</script>
+          </footer>"
 }
